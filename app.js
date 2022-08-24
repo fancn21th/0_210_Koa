5c4781403731ecc80ba683c6206dacbd47161a90
fancn21th <fancn21th@gmail.com>
2022-08-24 12:27:59 +0800

    router and ejs

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const Koa = require("koa");
 const KoaRouter = require("koa-router");
+const path = require("path");
+const render = require("koa-ejs");
 
 const app = new Koa();
 const router = new KoaRouter();
 
+// 简单的无路由处理
 // app.use(async (ctx) => (ctx.body = { msg: "hello there" }));
 
-router.get("/foo", (ctx) => (ctx.body = { msg: "hello there" }));
+render(app, {
+  root: path.join(__dirname, "views"),
+  layout: "layout",
+  viewExt: "html",
+  cache: false,
+  debug: false,
+});
+
+// 路由中间件
+router.get("/", async (ctx) => {
+  await ctx.render("index");
+});
+
+router.get("/foo", (ctx) => (ctx.body = { msg: "hello foo" }));
 
 app.use(router.routes()).use(router.allowedMethods());
 
